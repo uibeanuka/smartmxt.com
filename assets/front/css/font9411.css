64158ddffd950832804bf545a1062e02e3e98c02
uibeanuka <uibeanuka@gmail.com>
2024-01-12 15:36:58 +0100

    second commit

---
+++ assets/front/css/font9411.css
@@ -0,0 +1,16 @@
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+.form-label, .btn {
+    font-family: Manrope;
+}
+
+@media screen and (max-width: 375px) {
+    .change-language {
+        font-family: Manrope;
+    }
+}
